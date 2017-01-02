c94ec94d74d925b34580f6711a3b3c7a2a3659ad
Priyanka <priyanka.rupali@gmail.com>
2017-01-02 10:55:58 +0530

    First commit

---
+++ CharfromASCII.java
@@ -0,0 +1,12 @@
+package assignment.acadgild;
+
+public class CharfromASCII {
+
+	public static void main(String[] args) {
+		for(int i = 65; i <= 90; i++)
+		{
+			char character = (char) i;
+			System.out.println(i+"-"+character);
+		}
+	}
+}
